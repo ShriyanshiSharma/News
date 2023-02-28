d5d31540dc3514c352444e5cdea8df87e59a3079
Shriyanshi Sharma <shriyanshi.19b131153@abes.ac.in>
2023-02-28 22:33:44 +0530

    initial commit

---
+++ src/components/common/header/Head.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import logo from '../../assets/xyz-logo-1.png'
+// import ad from '../../assets/xyz.png'
+
+
+const Head = () => {
+    return(
+        <>
+        <section className='head'>
+        <div className='container flexSB paddingTB'>
+          <div className='logo'>
+            <img src={logo} alt='logo' />
+          </div>
+          <div className='ad'>
+            <img src="https://vignette.wikia.nocookie.net/create-logopedia/images/3/3a/XYZ_Logo.png/revision/latest/scale-to-width-down/640?cb=20160305041057" alt='ad' width="200px" />
+          </div>
+        </div>
+      </section>
+        </>
+    );
+}
+
+export default Head;

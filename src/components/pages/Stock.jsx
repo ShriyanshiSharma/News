46f9d443b18ab7db6abf60383c1ef33e0c19526f
Shriyanshi Sharma <shriyanshi.19b131153@abes.ac.in>
2023-03-01 22:34:05 +0530

    commit

--- src/components/pages/Stock.jsx
+++ src/components/pages/Stock.jsx
@@ -1,12 +1,31 @@
 import React from "react";
+import './style.css'
+import Display from "./Display";
+import {data } from '../../data';
+import { useState } from 'react';
 
 const Stock = () => {
-    return(
-        <>
-        <div className="common">
-            <h1>Stocks News</h1>
-        </div>
-        </>
-    );
+ 
+        const [item,setItems] = useState(data)
+
+    
+return(
+    <>
+    <div className="common">
+        <h1>Bitcoin News</h1>
+        {
+            data.map((item) => {
+                return<>
+                {
+                    <div className="display-detail">
+                    {((item.catego) === "Stock" ) ? <Display key={item.id} item = {item}/>:  (null)}
+                    </div>
+                }
+                </>
+            })
+        }
+    </div>
+    </>
+);
 }
 export default Stock;
